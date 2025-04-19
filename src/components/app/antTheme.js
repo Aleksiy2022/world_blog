033c314d726556bf26217174d5c77304b5d5c63c
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-19 22:01:52 +0300

    Added design tokens for antd.

---
+++ src/components/app/antTheme.js
@@ -0,0 +1,17 @@
+const theme = {
+  components: {
+    Pagination: {
+      itemActiveBg: '#1890FF',
+      itemBg: 'none',
+      colorPrimary: '#FFFFFF',
+      colorPrimaryHover: 'none',
+      controlHeight: 24,
+      fontSize: 12
+    },
+    Tag: {
+      defaultBg: '#FFFFFF',
+    }
+  },
+}
+
+export { theme }
