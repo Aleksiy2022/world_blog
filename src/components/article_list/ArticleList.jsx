21bcd9370353268eeb6e3d6355135cc6381b9946
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-19 11:06:40 +0300

    Added pagination.

--- src/components/article_list/ArticleList.jsx
+++ src/components/article_list/ArticleList.jsx
@@ -1,8 +1,27 @@
-function ArticleList() {
-  return (
-    <ul className={'list-reset'}>
+import { Pagination } from 'antd';
+import { useGetArticlesQuery } from '/src/redux/apiSlice.js'
 
-    </ul>
+function ArticleList() {
+  const { data: articles } = useGetArticlesQuery(5, 0)
+  console.log(articles)
+
+  return (
+    <>
+      <ul className={'list-reset'}>
+
+      </ul>
+      <Pagination align="center" defaultCurrent={1} total={articles?.articlesCount ? articles.articlesCount : 0} defaultPageSize={5} />
+    </>
+  )
+}
+
+function ArticlePreview() {
+
+
+  return (
+    <li>
+
+    </li>
   )
 }
 
