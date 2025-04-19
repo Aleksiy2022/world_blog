a904d54af8d1e9877f4a9244b1d5cff38fbb0d66
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-19 22:02:15 +0300

    Fixed bug.

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -5,7 +5,7 @@ const blogApiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://blog-platform.kata.academy/api' }),
   endpoints: (builder) => ({
     getArticles: builder.query({
-      query: (limit, offset) => `/articles?limit=${limit}&offset=${offset}`,
+      query: ({ limit, offset }) => `/articles?limit=${limit}&offset=${offset}`,
     }),
     getArticleBySlug: builder.query({
       query: (slug) => `/articles/${slug}`,
@@ -13,5 +13,5 @@ const blogApiSlice = createApi({
   })
 })
 
-export { blogApiSlice }
+export default blogApiSlice
 export const { useGetArticlesQuery } = blogApiSlice
