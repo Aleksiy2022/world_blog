9bd958c72d4af994c70086280ae79d839dd68eea
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-19 22:04:05 +0300

    Added antd config provider.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,15 +2,17 @@ import Header from '../header/Header.jsx'
 import ArticleList from '../article_list/ArticleList.jsx'
 import { Router, Route } from "react-router";
 import classes from './app.module.scss'
+import { theme } from './antTheme.js'
+import { ConfigProvider } from 'antd'
 
 function App() {
   return (
-    <>
+    <ConfigProvider theme={theme}>
       <Header />
       <main className={classes['main']}>
         <ArticleList />
       </main>
-    </>
+    </ConfigProvider>
   )
 }
 
