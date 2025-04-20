7f3f22d034d5a0ef0dc7486bccd381f55d2a3db8
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-20 10:27:13 +0300

    Added new rule.

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,21 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
-import prettier from 'eslint-plugin-prettier'
 import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   { ignores: ['node_modules', 'dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -21,7 +24,6 @@ export default [
     },
     plugins: {
       react: react,
-      prettier: prettier,
       import: importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
@@ -29,17 +31,14 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...prettier.configs.recommended.rules,
       ...importPlugin.configs.errors.rules,
       ...importPlugin.configs.warnings.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       indent: 'off',
-      endOfLine: 'auto',
       'react-hooks/exhaustive-deps': 'off',
-      'prettier/prettier': 'error',
-      'linebreak-style': ['off'],
+      'linebreak-style': [0, 'unix'],
       quotes: ['error', 'single'],
       semi: ['error', 'never'],
       'react/react-in-jsx-scope': 'off',
@@ -66,4 +65,5 @@ export default [
       },
     },
   },
+  eslintConfigPrettier,
 ]
