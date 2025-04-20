2cb3a94d1b5a96bae01d20811ac09dbc6c2a808e
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-20 10:28:07 +0300

    Deleted Routes component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
-import {BrowserRouter, Route, Routes} from 'react-router'
-import App from "./components/app/App.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import App from './components/app/App.jsx'
 import { store } from './redux/store.js'
 import './assets/styles/common.modules.scss'
 
-const root = document.getElementById("root");
+const root = document.getElementById('root')
 
 createRoot(root).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-        </Routes>
+        <App />
       </BrowserRouter>
     </Provider>
   </StrictMode>
-);
+)
