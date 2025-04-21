45f5d89f72d0146291536ca441952f324b112abb
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-21 23:34:45 +0300

    Added fields validation.

--- src/components/profile_forms/login_form/LoginForm.jsx
+++ src/components/profile_forms/login_form/LoginForm.jsx
@@ -1,26 +1,66 @@
 import { Button, Form, Input } from 'antd'
 import { Link } from 'react-router'
+import { useForm, Controller } from 'react-hook-form'
 
 import classes from '../profile-form.module.scss'
 
 function LoginForm() {
-  const [form] = Form.useForm();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div className={classes['form']}>
       <h4 className={classes['form__title']}>Sign in</h4>
-      <Form layout={'vertical'} form={form} style={{ width: '100%'}}>
-        <Form.Item label='Email address'>
-          <Input name='email' placeholder='Email address'></Input>
+      <Form layout={'vertical'} onFinish={handleSubmit(onSubmit)} style={{ width: '100%' }}>
+        <Form.Item
+          label="Email address"
+          validateStatus={errors.email ? 'error' : ''}
+          help={errors.email ? 'Email is required' : ''}
+        >
+          <Controller
+            name="email"
+            control={control}
+            rules={{ required: true, pattern: /^\S+@\S+\.\S+$/ }}
+            render={({ field }) => <Input {...field} placeholder="Email address" type="email" />}
+          />
         </Form.Item>
-        <Form.Item label='Password'>
-          <Input name='password' placeholder='Password'></Input>
+        <Form.Item
+          label="Password"
+          validateStatus={errors.password ? 'error' : ''}
+          help={errors.password ? errors.password.message : ''}
+        >
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: 'Password is required' }}
+            render={({ field }) => <Input.Password {...field} placeholder="Password" />}
+          />
         </Form.Item>
         <Form.Item>
-          <Button rootClassName={classes['form__btn']} type='primary' block>Login</Button>
+          <Button rootClassName={classes['form__btn']} type="primary" block htmlType="submit">
+            Login
+          </Button>
         </Form.Item>
       </Form>
-      <span className={classes['form__prompt']}>Dont`t have an account? <Link to='/sign-up' className={classes['form__link']}>Sign Un</Link>.</span>
+      <span className={classes['form__prompt']}>
+        Dont`t have an account?{' '}
+        <Link to="/sign-up" className={classes['form__link']}>
+          Sign Un
+        </Link>
+        .
+      </span>
     </div>
   )
 }
--- src/components/profile_forms/register_form/RegisterForm.jsx
+++ src/components/profile_forms/register_form/RegisterForm.jsx
@@ -1,35 +1,133 @@
 import { Button, Form, Input, Checkbox } from 'antd'
 import { Link } from 'react-router'
+import { useForm, Controller } from 'react-hook-form'
 
 import classes from '../profile-form.module.scss'
 
 function RegisterForm() {
-  const [form] = Form.useForm();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+      agree: true,
+    },
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div className={classes['form']}>
       <h4 className={classes['form__title']}>Create new account</h4>
-      <Form layout={'vertical'} form={form}>
-        <Form.Item label='Username'>
-          <Input name='username' placeholder='Username'></Input>
+      <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
+        <Form.Item
+          label="Username"
+          validateStatus={errors.username ? 'error' : ''}
+          help={errors.username ? 'Username is required' : ''}
+        >
+          <Controller
+            name="username"
+            control={control}
+            rules={{
+              required: true,
+              minLength: {
+                value: 3,
+                message: 'Your username needs to be at least 3 characters.',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Your username needs to be no more than 20 characters.',
+              },
+          }}
+            render={({ field }) => <Input {...field} placeholder="Username" />}
+          />
         </Form.Item>
-        <Form.Item label='Email address'>
-          <Input name='email' placeholder='Email address'></Input>
+
+        <Form.Item
+          label="Email address"
+          validateStatus={errors.email ? 'error' : ''}
+          help={errors.email ? 'Email is required' : ''}
+        >
+          <Controller
+            name="email"
+            control={control}
+            rules={{ required: true, pattern: /^\S+@\S+\.\S+$/ }}
+            render={({ field }) => <Input {...field} placeholder="Email address" type="email" />}
+          />
         </Form.Item>
-        <Form.Item label='Password'>
-          <Input name='password' placeholder='Password'></Input>
+
+        <Form.Item
+          label="Password"
+          validateStatus={errors.password ? 'error' : ''}
+          help={errors.password ? errors.password.message : ''}
+        >
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: true,
+              minLength: {
+                value: 6,
+                message: 'Your password needs to be at least 6 characters.',
+              },
+              maxLength: {
+                value: 40,
+                message: 'Your password needs to be no more than 40 characters.',
+              },
+          }}
+            render={({ field }) => <Input.Password {...field} placeholder="Password" />}
+          />
         </Form.Item>
-        <Form.Item label='Password'>
-          <Input name='repeatPassword' placeholder='Password'></Input>
+
+        <Form.Item
+          label="Repeat Password"
+          validateStatus={errors.repeatPassword ? 'error' : ''}
+          help={errors.repeatPassword ? errors.repeatPassword.message : ''}
+        >
+          <Controller
+            name="repeatPassword"
+            control={control}
+            rules={{
+              required: 'Please repeat your password',
+              validate: (value) => value === control._formValues.password || 'Passwords must match',
+            }}
+            render={({ field }) => <Input.Password {...field} placeholder="Password" />}
+          />
         </Form.Item>
+
+        <Form.Item validateStatus={errors.agree ? 'error' : ''} help={errors.agree ? 'You must agree to continue' : ''}>
+          <Controller
+            name="agree"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Checkbox {...field} checked={field.value} className={classes['form__checkbox']}>
+                I agree to the processing of my personal information
+              </Checkbox>
+            )}
+          />
+        </Form.Item>
+
         <Form.Item>
-          <Checkbox rootClassName={classes['form__checkbox']}>I agree to the processing of my personal information</Checkbox>
-        </Form.Item>
-        <Form.Item>
-          <Button rootClassName={classes['form__btn']} type='primary' block>Create</Button>
+          <Button className={classes['form__btn']} type="primary" block htmlType="submit">
+            Create
+          </Button>
         </Form.Item>
       </Form>
-      <span className={classes['form__prompt']}>Already have an account? <Link to='/sign-in' className={classes['form__link']}>Sign In</Link>.</span>
+      <span className={classes['form__prompt']}>
+        Already have an account?
+        <Link to="/sign-in" className={classes['form__link']}>
+          Sign In
+        </Link>
+        .
+      </span>
     </div>
   )
 }
