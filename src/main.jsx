6f65b0a93f609ab87b2738013ead82f46b776464
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-21 23:34:11 +0300

    Deleted unused import.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router'
 
 import App from './components/app/App.jsx'
 import { store } from './redux/store.js'
 import './assets/styles/common.modules.scss'
+import '@ant-design/v5-patch-for-react-19'
 
 const root = document.getElementById('root')
 
