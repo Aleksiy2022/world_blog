e8e8ef1157d870a913ef6d7c540591184cc21a50
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-21 22:24:49 +0300

    Deleted checkbox.

--- src/components/profile_forms/login_form/LoginForm.jsx
+++ src/components/profile_forms/login_form/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Checkbox } from 'antd'
+import { Button, Form, Input } from 'antd'
 import { Link } from 'react-router'
 
 import classes from '../profile-form.module.scss'
@@ -9,15 +9,12 @@ function LoginForm() {
   return (
     <div className={classes['form']}>
       <h4 className={classes['form__title']}>Sign in</h4>
-      <Form layout={'vertical'} form={form}>
+      <Form layout={'vertical'} form={form} style={{ width: '100%'}}>
         <Form.Item label='Email address'>
           <Input name='email' placeholder='Email address'></Input>
         </Form.Item>
         <Form.Item label='Password'>
           <Input name='password' placeholder='Password'></Input>
-        </Form.Item>
-        <Form.Item>
-          <Checkbox rootClassName={classes['form__checkbox']}>I agree to the processing of my personal information</Checkbox>
         </Form.Item>
         <Form.Item>
           <Button rootClassName={classes['form__btn']} type='primary' block>Login</Button>
