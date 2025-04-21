3146f20747286073987f336c67f9adeb7469f51e
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-21 22:25:37 +0300

    Added links to forms and styles for links.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router'
+import { Flex, } from 'antd'
 
 import classes from './header.module.scss'
 
@@ -10,6 +11,16 @@ function Header() {
           Realworld Blog
         </Link>
       </h1>
+      <Flex>
+        <Link to={'/sign-in'} className={`
+            ${classes['header__link']}
+            ${classes['header__link--sign-in']}
+          `}>Sign In</Link>
+        <Link to={'/sign-up'} className={`
+            ${classes['header__link']}
+            ${classes['header__link--sign-up']}
+          `}>Sign Up</Link>
+      </Flex>
     </header>
   )
 }
