7685b6ad3d4b3b2fc134240728e966b44d0a0f38
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-22 15:21:50 +0300

    Added auth queries.

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -10,8 +10,30 @@ const blogApiSlice = createApi({
     getArticleBySlug: builder.query({
       query: ({ slug }) => `/articles/${slug}`,
     }),
+    createNewUser: builder.mutation({
+      query: (userData) => ({
+        url: '/users',
+        method: 'POST',
+        body: userData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ loginData, jwt }) => ({
+        url: '/users/login',
+        method: 'POST',
+        body: loginData,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+      }),
+    }),
   }),
 })
 
 export default blogApiSlice
-export const { useGetArticlesQuery, useGetArticleBySlugQuery } = blogApiSlice
+export const { useGetArticlesQuery, useGetArticleBySlugQuery, useCreateNewUserMutation, useLoginMutation } =
+  blogApiSlice
