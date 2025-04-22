8e4e4e5af66240fb68797031ba642f1e4d2d3ed7
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-22 22:54:11 +0300

    Added authStatus.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,38 @@
 import { Link } from 'react-router'
 import { Flex } from 'antd'
+import { useSelector } from 'react-redux'
+
+import { selectAuth } from '../profile_forms/authSlice.js'
 
 import classes from './header.module.scss'
 
+
 function Header() {
+  const authStatus = useSelector(selectAuth)
+  const signIn = (
+    <Link
+      to={'/sign-in'}
+      className={`
+            ${classes['header__link']}
+            ${classes['header__link--sign-in']}
+          `}
+    >
+      Sign In
+    </Link>
+  )
+
+  const signUp = (
+    <Link
+      to={ authStatus ? '/' : '/sign-up'}
+      className={`
+            ${classes['header__link']}
+            ${ authStatus ? classes['header__link--log-out'] : classes['header__link--sign-up']}
+          `}
+    >
+      { authStatus ? 'Log Out' : 'Sign Up' }
+    </Link>
+  )
+
   return (
     <header className={classes['header']}>
       <h1>
@@ -12,24 +41,8 @@ function Header() {
         </Link>
       </h1>
       <Flex>
-        <Link
-          to={'/sign-in'}
-          className={`
-            ${classes['header__link']}
-            ${classes['header__link--sign-in']}
-          `}
-        >
-          Sign In
-        </Link>
-        <Link
-          to={'/sign-up'}
-          className={`
-            ${classes['header__link']}
-            ${classes['header__link--sign-up']}
-          `}
-        >
-          Sign Up
-        </Link>
+        { signIn }
+        { signUp }
       </Flex>
     </header>
   )
