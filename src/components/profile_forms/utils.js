5290bd11faa51073f7db84ada52c3f9437c8b57b
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-22 22:45:27 +0300

    Deleted unused func.

--- src/components/profile_forms/utils.js
+++
@@ -1,7 +0,0 @@
-function getJwtExpiration(token) {
-  const payloadBase64 = token.split('.')[1]
-  const payload = JSON.parse(atob(payloadBase64))
-  return payload.exp ? new Date(payload.exp * 1000) : null
-}
-
-export { getJwtExpiration }
