9018c845cd91ee46c0f23d7d16a250e7b2f6735a
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-24 23:37:05 +0300

    Added styles for create and edit article component.

---
+++ src/components/article_edit_create_form/antdTheme.js
@@ -0,0 +1,17 @@
+const theme = {
+  token: {
+    fontWeightStrong: 500,
+    controlHeight: 40.38,
+    colorPrimary: '#1890FF',
+  },
+  components: {
+    Typography: {
+      titleMarginBottom: 0,
+    },
+    Form: {
+      itemMarginBottom: 21,
+    },
+  }
+}
+
+export { theme }
