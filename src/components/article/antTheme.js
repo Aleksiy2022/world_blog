3e30074f1b5e48e990e0ac4f4b0668199357fa45
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-25 19:20:52 +0300

    Added delete and edit btns styles.

---
+++ src/components/article/antTheme.js
@@ -0,0 +1,12 @@
+const editBtn = {
+  token: {
+    colorPrimary: '#52C41A',
+  },
+}
+const deleteBtn = {
+  token: {
+    colorPrimary: '#F5222D',
+    marginXXS: 0,
+  },
+}
+export { editBtn, deleteBtn }
