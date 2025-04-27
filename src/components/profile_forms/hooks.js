9e7b3dcdfd3e77fa75a73770883de720b90e6501
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-27 09:42:31 +0300

    feat: add useLogin hook

---
+++ src/components/profile_forms/hooks.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+
+import { getJwtExpiration } from './utils.js'
+import { setAuthorized } from './authSlice.js'
+
+function useLogin({ user, dispatch, navigate, isRegister = false }, refetch) {
+  console.log('isRegister: ', isRegister)
+  console.log('user: ', user)
+  useEffect(() => {
+    if (user) {
+      const jwt = user.user.token
+      const expiresAt = getJwtExpiration(jwt)
+      localStorage.setItem('blogAuthTokenData', JSON.stringify({ jwt: jwt, expiresAt: expiresAt, email: user.email }))
+      if (isRegister) {
+        refetch()
+      }
+      dispatch(setAuthorized())
+      navigate('/')
+    }
+  }, [user])
+}
+
+export { useLogin }
